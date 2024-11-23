a485ca43986c6bff6326347cc00d496140435971
Santy Cierco <santidisturbios@gmail.com>
2024-11-23 11:16:22 +0100

    new version of profilesII

--- 5_ProfilesII/src/main/java/org/example/Main.java
+++ 5_ProfilesII/src/main/java/org/example/Main.java
@@ -7,31 +7,27 @@ import java.util.Properties;
 public class Main {
     public static void main(String[] args) {
         try {
-            // Create Properties object
             Properties properties = new Properties();
-            
-            // Load the properties file from resources
+
             try (InputStream input = Main.class.getClassLoader()
                     .getResourceAsStream("db.properties")) {
-                
+
                 if (input == null) {
                     System.out.println("Sorry, unable to find db.properties");
                     return;
                 }
 
-                // Load properties from input stream
                 properties.load(input);
 
-                // Display the properties
                 System.out.println("Database Configuration:");
                 System.out.println("Driver: " + properties.getProperty("driverClassName"));
                 System.out.println("URL: " + properties.getProperty("url"));
                 System.out.println("Username: " + properties.getProperty("username"));
                 System.out.println("Password: " + properties.getProperty("password"));
-
             }
         } catch (IOException e) {
             e.printStackTrace();
         }
     }
 }
+
