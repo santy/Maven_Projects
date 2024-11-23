4d7362286cc7a312059b00b8ed2afa09f4d0d668
Santy Cierco <santidisturbios@gmail.com>
2024-11-23 15:29:33 +0100

    create ProfilesIII

---
+++ 6_PerfilesIII/src/test/java/org/example/TestMain.java
@@ -0,0 +1,15 @@
+package org.example;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+public class TestMain {
+
+    @Test
+    @DisplayName("Testing Main")
+    public void testMain() {
+        assertEquals(1, 1);
+    }
+}
